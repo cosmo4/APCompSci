83ecbd7756a7a03edf8074555b38ca3416f43024
cosmo4 <44450832+cosmo4@users.noreply.github.com>
2018-11-01 11:48:21 -0600

    Add files via upload

--- FractionTester.java
+++ FractionTester.java
@@ -1,19 +1,12 @@
-public class FractionTester {
-	
-	public static void main(String[] args) {
-		Fraction f = new Fraction("3/4");
-		Fraction g = new Fraction("1/5");
-		Fraction[] myFractions = new Fraction[5];
-		
-		// Add the fractions, store the result
-		Fraction sum = f.add(g);
-		
-		myFractions[0] = f;
-		myFractions[1] = g;
-		myFractions[4] = sum;
-		
-		// Print the result
-		System.out.println(myFractions[4].toPrettyString());
 
+public class FractionTester {
+
+	public static void main(String[] args) {
+		
+		Fraction myFraction = new Fraction("1/3");
+		Fraction yourFraction = new Fraction("4/13");
+		
+		System.out.println("Numerator: " + myFraction.getNumerator() + ", Denominator: " + myFraction.getDenominator());
+		System.out.println("Numerator 2: " + yourFraction.getNum() + ", Denominator 2: " + yourFraction.getDen());
 	}
 }
